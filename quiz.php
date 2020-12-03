921e8ef73c0aaddb8fd4217c8a91abbbaaebc7ba
Sameur <bouzgarrousameur@gmail.com>
2020-12-03 21:24:23 +0100

    test

---
+++ quiz.php
@@ -0,0 +1,17 @@
+< doctype html>
+<html lang="fr">
+<meta charset="Utf8" />
+<head>
+</head>
+<body> 
+    votre réponse est donc :
+<?php
+$som =0? ;
+foreach ($-get as $numero => $reponse {
+    echo <li> , $numero," : " ,$reponse "</li>" ;
+    $som = $som+$reponse ;
+}
+echo "vous avez eu" ,$som, "reponse"
+?>
+</body>
+</html>
